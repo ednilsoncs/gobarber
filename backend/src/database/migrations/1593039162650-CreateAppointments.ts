5f57cef665da94e581cce67cd98294a3a5e0cb79
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-24 22:55:21 -0300

    feat: added relasionship between Appointment and Users

--- backend/src/database/migrations/1593039162650-CreateAppointments.ts
+++ backend/src/database/migrations/1593039162650-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593039162650
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',

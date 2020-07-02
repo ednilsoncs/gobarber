adcd9aa025c9e32a4b352548581c3660a0a3703b
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-01 23:41:59 -0300

    feat: added Component Tooltip for exibi error mensagem

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from "styled-components";
+import Tooltip from "../Tooltip";
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +22,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -31,6 +39,7 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
 
   input {
     flex: 1;
@@ -44,5 +53,23 @@ export const Container = styled.div<ContainerProps>`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
---
+++ web/src/components/Tooltip/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "./styles";
+
+interface TooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ title, className, children }) => (
+  <Container className={className}>
+    {children}
+    <span>{title}</span>
+  </Container>
+);
+
+export default Tooltip;

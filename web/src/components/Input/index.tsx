c8d41d5db8d11439be5a130c9362dbdd78ce2000
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-01 21:38:16 -0300

    chore: input added integration with unform

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,5 +1,8 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, {
+  InputHTMLAttributes, useEffect, useRef, useState, useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,14 +10,47 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input
-      {...rest}
-      type="text"
-    />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const {
+    fieldName, defaultValue, error, registerField,
+  } = useField(name);
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    if (inputRef.current?.value) {
+      setIsFilled(true);
+    } else {
+      setIsFilled(false);
+    }
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        ref={inputRef}
+        defaultValue={defaultValue}
+        {...rest}
+        type="text"
+      />
+    </Container>
+  );
+};
 
 export default Input;

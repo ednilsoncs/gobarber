da56de986a1f77e7f5a087049f5e616324fe0c22
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-02 01:23:14 -0300

    chore: added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Response, Request, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -7,6 +8,7 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.directory));

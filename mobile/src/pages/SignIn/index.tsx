ace7fbf867df784008487ca2a7c242538a7433b7
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-05 20:24:37 -0300

    feat: added input and button component

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { Image } from 'react-native';
 import logoImg from '../../assets/logo.png';
 import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('OK');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };

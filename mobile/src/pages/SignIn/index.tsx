db3b2d64315abb5a4f5a0d31f1f3343f54384476
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-05 17:09:11 -0300

    project: configure project fonts and import .png files

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Container } from './styles';
+import { Image } from 'react-native';
+import logoImg from '../../assets/logo.png';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
 };
 
 export default SignIn;

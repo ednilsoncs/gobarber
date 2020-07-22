427c3185679b992acb61e6d0ec40bf330a63190b
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-22 19:36:12 -0300

    chore: added Template Email Provider

---
+++ backend/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,9 @@
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+
+class FakeMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
+    return template;
+  }
+}
+export default FakeMailTemplateProvider;
--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -12,4 +14,9 @@ container.registerSingleton<IStorageProvider>(
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'StorageProvider',
+  HandlebarsMailTemplateProvider,
 );

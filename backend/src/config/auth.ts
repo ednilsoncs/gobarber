051f20ae86566832f22e88bf68685df79af39167
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-27 21:07:56 -0300

    feat: added environment variable

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'af8e8783b71af47eccb6241423bd58c7',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express, { Response, Request, NextFunction } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';

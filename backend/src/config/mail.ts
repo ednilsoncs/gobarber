cac1098328274011c1d4701f9580d1f081c895e5
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-27 23:23:18 -0300

    chore: added integration with AWS SAS

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'sas';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVE || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'ednilsonsantos@gmail.com',
+      name: 'Ednilson Cardoso dos Santos',
+    },
+  },
+} as IMailConfig;

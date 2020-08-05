cf3a0a5a59845b3373307e6111e102090e3d7b8f
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-08-04 21:34:14 -0300

    fix: api change to use classToClass in ProvidersController and ListProvidersService

--- backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+import { classToClass } from 'class-transformer';
 
 export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -10,6 +11,6 @@ export default class ProvidersController {
     const appointment = await createAppointment.execute({
       user_id,
     });
-    return response.json(appointment);
+    return response.json(classToClass(appointment));
   }
 }
--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -26,7 +27,10 @@ class ListProvidersService {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
       });
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;

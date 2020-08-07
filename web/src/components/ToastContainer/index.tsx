9a77df1ba51f780cbd3cd96112dd78614df1b1cb
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-08-06 21:03:16 -0300

    style(web): eslint rules applied

--- web/src/components/ToastContainer/index.tsx
+++ web/src/components/ToastContainer/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { useTransition } from "react-spring";
-import { Container } from "./styles";
-import { ToastMessage } from "../../hooks/toast";
-import Toast from "./Toast";
+import React from 'react';
+import { useTransition } from 'react-spring';
+import { Container } from './styles';
+import { ToastMessage } from '../../hooks/toast';
+import Toast from './Toast';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
@@ -11,21 +11,21 @@ interface ToastContainerProps {
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   const messagesWithTransitions = useTransition(
     messages,
-    (message) => message.id,
+    message => message.id,
     {
       from: {
-        right: "-120%",
+        right: '-120%',
         opacity: 0,
       },
       enter: {
-        right: "0%",
+        right: '0%',
         opacity: 1,
       },
       leave: {
-        right: "-120%",
+        right: '-120%',
         opacity: 0,
       },
-    }
+    },
   );
   return (
     <Container>
